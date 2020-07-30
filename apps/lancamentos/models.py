ac8123b049c7403a77b3556973e6171b3cfef977
Jefeson Alves <jefesonbezerra@gmail.com>
2020-07-29 21:07:29 -0300

    Adicionado campos de data e hora.
    Adicionado campos de data e hora no models lançamentos.

--- apps/lancamentos/models.py
+++ apps/lancamentos/models.py
@@ -4,9 +4,11 @@ from apps.tear.models import Tear
 from apps.eficiencia.models import Eficiencia
 
 class Lancamentos(models.Model):
+    data = models.DateField(null=True)
+    hora = models.TimeField(null=True)
     tear = models.ForeignKey(Tear, null=False, blank=False, on_delete=models.PROTECT)
     eficiencia = models.ForeignKey(Eficiencia, null=False, blank=False, on_delete=models.PROTECT)
-    evento = models.ForeignKey(Evento, null=False, blank=False, on_delete=models.PROTECT)    
+    evento = models.ForeignKey(Evento, null=False, blank=False, on_delete=models.PROTECT)   
 
     class Meta:
         verbose_name_plural = "01 - Lançamentos"
--- sistear/settings.py
+++ sistear/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
